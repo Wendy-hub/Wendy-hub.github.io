033f7c7e568fda031da84fe07251034c2bc113c7
Eric Wang <75363363+Wendy-hub@users.noreply.github.com>
2023-11-18 18:16:20 +0800

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,32 @@
+var config = {
+    texts: [
+        "Hello 阿布都拉",
+        "我的朋友😊",
+        "我很喜欢你的名字btw!",
+        "地球又一次转到了太阳第一次照你的地方;)",
+        "今天没有流星🌠",
+        "但是是可以许愿的日子",
+        "每天有每天的快乐",
+        "今天是生日快乐!",
+        "祝你身体健康，学习进步",
+        "成长不期而遇 未来可期!",
+        "生日快乐🎂",
+        "Okay, 现在回来告诉我你喜不喜欢吧;)",
+    ],
+
+    imgs: {
+        "Hello 阿布都拉": "./imgs/avator.jpg",
+        "Okay, 现在回来告诉我你喜不喜欢吧;)": "./imgs/avator.jpg",
+    },
+
+    desc: {
+        turn_on: "开始吧?",
+        play: "音乐Music!",
+        bannar_coming: "让我们装扮一下!",
+        balloons_flying: "好像少点东西...",
+        cake_fadein: "蛋糕呢?",
+        light_candle: "来点蜡烛吧!",
+        wish_message: "生日快乐!!!!!!",
+        story: "A MESSAGE FOR YOU",
+    }
+};
